4861694a6e830d63aad2f5444bc9e25720e0b108
Mohammed Taher <mohammed.taher2007@gmail.com>
2023-03-12 21:41:08 +0300

    Added Noice to neovim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -340,6 +340,21 @@ use({
   "lukas-reineke/lsp-format.nvim",
 })
 
+use({
+  "folke/noice.nvim",
+  config = function()
+    require('user/plugins/noice')
+  end,
+  requires = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  }
+})
+
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
